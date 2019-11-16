acc8f90bb1dfb7551e933695b9fdbf91cc9ef62e
WJXXBSH <zyb_1998@outlook.com>
2019-11-16 15:09:16 +0800

    libra: Bind mount loader config for media swcodec
     * The AOSP ld config for it does not take legacy devices
       into account and crashes when using sw decoder. Add a new
       one which removes sphal rules and bind mount it to /apex.

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ PRODUCT_PACKAGES += \
     com.dsi.ant.antradio_library \
     libantradio
 
+# APEX
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/vendor/etc/aanc_tuning_mixer.txt \
