ec0a26c6dba8525b1fe74a5cc34c77b1f28d6c81
WJXXBSH <zyb_1998@outlook.com>
2019-11-16 15:16:32 +0800

    libra: Drop mediaextractor seccomp policy
    * These syscalls are now allowed by default.

--- device.mk
+++ device.mk
@@ -293,8 +293,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_COPY_FILES += \
