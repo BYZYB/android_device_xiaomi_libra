cea8e6e49cc1c80efb9548c57cea7580bbe59822
WJXXBSH <zyb_1998@outlook.com>
2019-11-16 13:23:16 +0800

    libra: Remove deprecated display flags

--- system_prop.mk
+++ system_prop.mk
@@ -72,7 +72,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196610
+    ro.opengles.version=196610 \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_virtual_display_dimension=2048
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
