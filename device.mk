f9154ddf976c762f2243c9565be5ae043ce72231
WJXXBSH <zyb_1998@outlook.com>
2019-11-22 15:30:48 +0800

    libra: Disable interaction boost
    * Does not work correctly with stock perf stack, switching
      profiles do not have any real effect at all.

--- device.mk
+++ device.mk
@@ -236,8 +236,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey \
-    android.hardware.drm@1.1-service.widevine \
+    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.2-service.widevine \
     libc2dcolorconvert \
     libdivxdrmdecrypt \
     libdrmclearkeyplugin \
