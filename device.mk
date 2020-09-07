ee32d1d67100e22458bf92823f79f359f24d8c43
WJXXBSH <zyb_1998@outlook.com>
2020-09-07 13:55:42 +0800

    libra: Remove prebuilt busybox binary
    The prebuilt busybox is only for doing fstrim after each boot, which wastes 1.95MB free space in "/system".
    According to documentations, the "disacrd" mount option in fstab.qcom has already enabled Continuous TRIM.
    That's why doing fstrim manually returns "0 bytes trimmed" each time, which has no effect after all.

--- device.mk
+++ device.mk
@@ -66,11 +66,6 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Busybox
-# This could be removed if busybox is not needed in your rom.
-# The binary here is fetched from "https://github.com/Magisk-Modules-Repo/busybox-ndk/blob/master/busybox-arm64-selinux".
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/rootdir/bin/busybox:system/xbin/busybox
-
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
