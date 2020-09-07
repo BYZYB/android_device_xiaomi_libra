f5b6fee3cd8539f95dacec3011c89d9ed5fa34a0
WJXXBSH <zyb_1998@outlook.com>
2020-09-07 13:55:42 +0800

    libra: Remove prebuilt busybox binary
    The prebuilt busybox is only for doing fstrim after each boot, which wastes 1.95MB free space in "/system".
    According to documentations, the "disacrd" mount option in fstab.qcom has already enabled Continuous TRIM.
    That's why doing fstrim manually returns "0 bytes trimmed" each time, which has no effect after all.

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -48,11 +48,6 @@ rm -rf /data/media/0/Pictures/.thumbnails/*
 rm -rf /data/user_de/*/*/cache/*
 rm -rf /data/user_de/*/*/code_cache/*
 
-# Do fstrim for all partitions
-for path in /*; do
-    /system/xbin/busybox fstrim -v $path
-done
-
 # Enable SELinux when root access is disabled
 if [ $(getprop persist.sys.root_access) -le 0 ]; then
     setenforce 1
