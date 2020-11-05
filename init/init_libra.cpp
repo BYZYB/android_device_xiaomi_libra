65d54c6f8e33c9d8f614020fd1765c1ba71314d7
WJXXBSH <zyb_1998@outlook.com>
2020-11-05 11:16:46 +0800

    libra: Refactor libinit
    Use fewer variables as well as some other clean-ups.

--- init/init_libra.cpp
+++ init/init_libra.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2019, The LineageOS Project
+   Copyright (c) 2020, The LineageOS Project
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -25,111 +25,99 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include "property_service.h"
 #include <fstream>
 #include <sys/sysinfo.h>
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 
-#include "property_service.h"
 #include <sys/_system_properties.h>
 
-using android::init::property_set;
+#define AQUA_BOARD_ID 30
+#define LIBRA_BOARD_ID 12
+#define BOARD_ID_PATH "/proc/device-tree/qcom,board-id"
 
-void property_override(char const prop[], char const value[])
+using std::ifstream;
+
+void property_override(const char *prop, const char *value)
 {
-    prop_info *pi;
+    prop_info *info = (prop_info *)__system_property_find(prop);
 
-    pi = (prop_info *)__system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
+    if (info)
+    {
+        __system_property_update(info, value, strlen(value));
+    }
     else
+    {
         __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
 }
 
-void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+inline void property_override_dual(const char *system_prop, const char *vendor_prop, const char *value)
 {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
 }
 
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
-char const *ram;
+uint8_t update_board_id()
+{
+    uint8_t board_id;
 
-uint8_t board_id;
+    /*
+      qcom,board-id contains two 4-byte numbers,
+      For libra, 00 00 00 0c and 00 00 00 00.
+      For aqua, 00 00 00 1e and 00 00 00 00.
+     */
+    ifstream board_id_file(BOARD_ID_PATH, ifstream::binary);
+    board_id_file.seekg(3); // Shift past the first 3 bytes, and only read the 4th one.
+    board_id_file.read(reinterpret_cast<char *>(&board_id), 1);
+
+    return board_id;
+}
 
 void set_init_values()
 {
     struct sysinfo sys;
-
     sysinfo(&sys);
 
     if (sys.totalram > 2048ull * 1024 * 1024)
     {
         // from - phone-xxhdpi-3072-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "288m";
-        heapsize = "768m";
-        heapminfree = "512k";
-        ram = "3g";
+        property_override("dalvik.vm.heapgrowthlimit", "288m");
+        property_override("dalvik.vm.heapminfree", "512k");
+        property_override("dalvik.vm.heapsize", "768m");
+        property_override("dalvik.vm.heapstartsize", "8m");
+        property_override("ro.product.ram", "3g");
     }
     else
     {
         // from - phone-xxhdpi-2048-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heapminfree = "2m";
-        ram = "2g";
+        property_override("dalvik.vm.heapgrowthlimit", "192m");
+        property_override("dalvik.vm.heapminfree", "2m");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("ro.product.ram", "2g");
     }
-}
-
-#define BOARD_ID_PATH "/proc/device-tree/qcom,board-id"
-#define LIBRA_BOARD_ID 12
-#define AQUA_BOARD_ID 30
-
-void set_board_id()
-{
-    /*
-      qcom,board-id contains 2 4-byte numbers,
-      For libra, 00 00 00 0c and 00 00 00 00.
-      For aqua, 00 00 00 1e and 00 00 00 00.
-     */
-    std::ifstream board_id_file(BOARD_ID_PATH, std::ifstream::binary);
-    /*
-      Shift past the first 3 bytes, and only read the 4th one.
-     */
-    board_id_file.seekg(3);
-    board_id_file.read(reinterpret_cast<char *>(&board_id), 1);
 }
 
 void vendor_load_properties()
 {
     set_init_values();
 
-    property_set("dalvik.vm.heapstartsize", heapstartsize);
-    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", heapsize);
-    property_set("dalvik.vm.heaptargetutilization", "0.75");
-    property_set("dalvik.vm.heapminfree", heapminfree);
-    property_set("dalvik.vm.heapmaxfree", "8m");
-    property_set("ro.product.ram", ram);
+    property_override("dalvik.vm.heapmaxfree", "8m");
+    property_override("dalvik.vm.heaptargetutilization", "0.75");
 
-    set_board_id();
-
-    switch (board_id)
+    switch (update_board_id())
     {
     case LIBRA_BOARD_ID:
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi-4c");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "libra");
         property_override("ro.build.product", "libra");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "libra");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi-4c");
         break;
     case AQUA_BOARD_ID:
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi-4s");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "aqua");
         property_override("ro.build.product", "aqua");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "aqua");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi-4s");
         break;
     }
 }
