04a7bb6e16c8e22ec227cf180e791a8225bca227
WJXXBSH <zyb_1998@outlook.com>
2020-12-03 20:42:25 +0800

    libra: Set fpc parameters with libinit
    If the current device is Mi-4s, add/set fpc parameters dynamically with libinit_libra.
    This way should bypass the unnecessary fingerprint configs on Mi-4c.
    
    Also did some code clean-ups to libinit_libra.

--- init/init_libra.cpp
+++ init/init_libra.cpp
@@ -25,13 +25,12 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "property_service.h"
-#include <fstream>
-#include <sys/sysinfo.h>
-
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 
+#include "property_service.h"
+#include <fstream>
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 
 #define AQUA_BOARD_ID 30
 #define LIBRA_BOARD_ID 12
@@ -43,23 +42,23 @@ void property_override(const char *prop, const char *value)
 {
     prop_info *info = (prop_info *)__system_property_find(prop);
 
-    if (info)
+    if (info) // Update existing property
     {
         __system_property_update(info, value, strlen(value));
     }
-    else
+    else // Create new property
     {
         __system_property_add(prop, strlen(prop), value, strlen(value));
     }
 }
 
-inline void property_override_dual(const char *system_prop, const char *vendor_prop, const char *value)
+inline void property_override(const char *system_prop, const char *vendor_prop, const char *value)
 {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
 }
 
-uint8_t update_board_id()
+uint8_t get_board_id()
 {
     uint8_t board_id;
 
@@ -69,55 +68,62 @@ uint8_t update_board_id()
       For aqua, 00 00 00 1e and 00 00 00 00.
      */
     ifstream board_id_file(BOARD_ID_PATH, ifstream::binary);
-    board_id_file.seekg(3); // Shift past the first 3 bytes, and only read the 4th one.
+    board_id_file.seekg(3); // Shift past the first 3 bytes and only read the 4th one
     board_id_file.read(reinterpret_cast<char *>(&board_id), 1);
 
     return board_id;
 }
 
-void set_init_values()
+void vendor_load_properties()
 {
-    struct sysinfo sys;
-    sysinfo(&sys);
-
-    if (sys.totalram > 2048ull * 1024 * 1024)
+    switch (get_board_id())
     {
-        // from - phone-xxhdpi-3072-dalvik-heap.mk
+    case LIBRA_BOARD_ID:
+    {
+        struct sysinfo sys;
+        sysinfo(&sys);
+
+        // Set memory parameters
+        if (sys.totalram > 2048ull * 1024 * 1024) // Mi-4c with 3GB RAM
+        {
+            property_override("dalvik.vm.heapgrowthlimit", "288m");
+            property_override("dalvik.vm.heapminfree", "512k");
+            property_override("dalvik.vm.heapsize", "768m");
+            property_override("dalvik.vm.heapstartsize", "8m");
+            property_override("ro.product.ramsize", "3g");
+        }
+        else // Mi-4c with 2GB RAM
+        {
+            property_override("dalvik.vm.heapgrowthlimit", "192m");
+            property_override("dalvik.vm.heapminfree", "2m");
+            property_override("dalvik.vm.heapsize", "512m");
+            property_override("dalvik.vm.heapstartsize", "16m");
+            property_override("ro.product.ramsize", "2g");
+        }
+
+        break;
+    }
+
+    case AQUA_BOARD_ID:
+    {
+        // Set device info for Mi-4s
+        property_override("ro.build.product", "aqua");
+        property_override("ro.product.device", "ro.vendor.product.device", "aqua");
+        property_override("ro.product.model", "ro.vendor.product.model", "Mi-4s");
+
+        // Set memory parameters
         property_override("dalvik.vm.heapgrowthlimit", "288m");
         property_override("dalvik.vm.heapminfree", "512k");
         property_override("dalvik.vm.heapsize", "768m");
         property_override("dalvik.vm.heapstartsize", "8m");
         property_override("ro.product.ramsize", "3g");
-    }
-    else
-    {
-        // from - phone-xxhdpi-2048-dalvik-heap.mk
-        property_override("dalvik.vm.heapgrowthlimit", "192m");
-        property_override("dalvik.vm.heapminfree", "2m");
-        property_override("dalvik.vm.heapsize", "512m");
-        property_override("dalvik.vm.heapstartsize", "16m");
-        property_override("ro.product.ramsize", "2g");
-    }
-}
 
-void vendor_load_properties()
-{
-    set_init_values();
+        // Set fingerprint parameters
+        property_override("ro.frp.pst", "/dev/block/bootdevice/by-name/config");
+        property_override("ro.hardware.fingerprint", "fpc");
+        property_override("sys.fpc.tu.disabled", "0");
 
-    property_override("dalvik.vm.heapmaxfree", "8m");
-    property_override("dalvik.vm.heaptargetutilization", "0.75");
-
-    switch (update_board_id())
-    {
-    case LIBRA_BOARD_ID:
-        property_override("ro.build.product", "libra");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "libra");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi-4c");
         break;
-    case AQUA_BOARD_ID:
-        property_override("ro.build.product", "aqua");
-        property_override_dual("ro.product.device", "ro.vendor.product.device", "aqua");
-        property_override_dual("ro.product.model", "ro.vendor.product.model", "Mi-4s");
-        break;
+    }
     }
 }
