ac1d46a4079a652c7fa5d998389299be124fb48e
WJXXBSH <zyb_1998@outlook.com>
2020-12-14 13:59:15 +0800

    libra: Remove unused kernel offset values
    Also update storage cleaning rules and dex2oat properties.

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -1,26 +1,12 @@
 #!/vendor/bin/sh
+# Enable ADB root when needed
+if [ $(cat /data/adbroot/enabled) -gt 0 ]; then
+    setprop service.adb.root 1
+    setprop ctl.restart adbd
+fi
+
 # Remove useless directories
-rm -rf /data/anr
-rm -rf /data/lineageos_updates
-rm -rf /data/local/traces
-rm -rf /data/misc/boottrace
-rm -rf /data/misc/gcov
-rm -rf /data/misc/perfprofd
-rm -rf /data/misc/profman
-rm -rf /data/misc/trace
-rm -rf /data/misc/update_engine
-rm -rf /data/misc/update_engine_log
-rm -rf /data/misc/wmtrace
-rm -rf /data/nfc
-rm -rf /data/ota
-rm -rf /data/ss
-rm -rf /data/ssh
-rm -rf /data/system/dropbox
-rm -rf /data/system/heapdump
-rm -rf /data/system/perfd
-rm -rf /data/thermal
-rm -rf /data/tombstones
-rm -rf /data/vendor/tombstones
+rm -rf /data/anr /data/lineageos_updates /data/local/traces /data/misc/boottrace /data/misc/gcov /data/misc/perfprofd /data/misc/profman /data/misc/trace /data/misc/update_engine /data/misc/update_engine_log /data/misc/wmtrace /data/nfc /data/ota /data/per_boot /data/rollback /data/rollback-observer /data/ss /data/ssh /data/system/dropbox /data/system/heapdump /data/system/perfd /data/thermal /data/tombstones /data/vendor/tombstones
 
 # Remove useless files
 find /data/data/com.tencent.mm/MicroMsg/*/appbrand/* -mtime +3 -exec rm -rf {} \;
@@ -29,23 +15,4 @@ find /data/data/com.tencent.mm/MicroMsg/*/image*/* -mtime +3 -exec rm -rf {} \;
 find /data/media/0/Android/data/com.tencent.mm/MicroMsg/*/* -mtime +3 -exec rm -rf {} \;
 find /data/media/0/tencent/MobileQQ*/*/* -mtime +3 -exec rm -rf {} \;
 find /data/media/0/tencent/MobileQQ*/*/*/* -mtime +3 -exec rm -rf {} \;
-rm -f /data/system/uiderrors.txt
-rm -rf /data/backup/pending/*
-rm -rf /data/data/*/app_webview_*
-rm -rf /data/data/*/app_webview*/*/*/CacheStorage/*
-rm -rf /data/data/*/app_webview*/*/*/ScriptCache/*
-rm -rf /data/data/*/cache/*
-rm -rf /data/data/*/code_cache/*
-rm -rf /data/data/com.microsoft.office*/files/Microsoft/Office/*/*
-rm -rf /data/data/com.microsoft.office*/files/temp*/*
-rm -rf /data/local/tmp/*
-rm -rf /data/media/0/*/.thumbnails/*
-rm -rf /data/media/0/Android/data/*/cache/*
-rm -rf /data/user_de/*/*/cache/*
-rm -rf /data/user_de/*/*/code_cache/*
-
-# Enable ADB root when needed
-if [ $(cat /data/adbroot/enabled) -gt 0 ]; then
-    setprop service.adb.root 1
-    setprop ctl.restart adbd
-fi
+rm -rf /data/backup/pending/* /data/data/*/app_webview_* /data/data/*/app_webview*/*/*/CacheStorage/* /data/data/*/app_webview*/*/*/ScriptCache/* /data/data/*/cache/* /data/data/*/code_cache/* /data/data/com.microsoft.office*/files/Microsoft/Office/*/* /data/data/com.microsoft.office*/files/temp*/* /data/local/tmp/* /data/media/0/*/.thumbnails/* /data/media/0/Android/data/*/cache/* /data/system/uiderrors.txt /data/user_de/*/*/cache/* /data/user_de/*/*/code_cache/*
