ddd2e59a14bca8694120507e5235f0168156bb85
Vijay Venkatraman <vijaykv@google.com>
2021-02-02 19:11:58 +0800

    libra: gps: Replacing copy headers with header libraries
    Bug: 33241851
    Test: Build target and pass CTS location tests
    Change-Id: I75db5e6de3c4194a6a46e0d37fcd899ea1d1db04

--- gps/loc_api/libloc_api_50001/Android.mk
+++ gps/loc_api/libloc_api_50001/Android.mk
@@ -45,20 +45,20 @@ LOCAL_C_INCLUDES:= \
     $(LOCAL_PATH) \
     $(TARGET_OUT_HEADERS)/libflp
 
-LOCAL_COPY_HEADERS_TO:= libloc_eng/
-LOCAL_COPY_HEADERS:= \
-   LocEngAdapter.h \
-   loc.h \
-   loc_eng.h \
-   loc_eng_xtra.h \
-   loc_eng_ni.h \
-   loc_eng_agps.h \
-   loc_eng_msg.h \
-   loc_eng_log.h
+LOCAL_HEADER_LIBRARIES := \
+    libgps.utils_headers \
+    libloc_core_headers
 
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libloc_eng_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+
+include $(BUILD_HEADER_LIBRARY)
 
 include $(CLEAR_VARS)
 
@@ -106,6 +106,10 @@ LOCAL_C_INCLUDES:= \
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_RELATIVE_PATH := hw
+
+LOCAL_HEADER_LIBRARIES := \
+    libgps.utils_headers \
+    libloc_core_headers
 
 include $(BUILD_SHARED_LIBRARY)
 
