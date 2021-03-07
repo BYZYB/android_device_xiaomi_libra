df7ea6f62f3ad35a73408c8e92f11e18410d526b
WJXXBSH <zyb_1998@outlook.com>
2021-03-07 11:47:29 +0800

    libra: Add support for mdss livedisplay driver
    The sysfs based DisplayColorCalibration support (/sys/class/graphics/fbX/rgb) has been implemented in the kernel.

--- device.mk
+++ device.mk
@@ -182,7 +182,7 @@ PRODUCT_PACKAGES += android.hardware.keymaster@3.0-impl
 PRODUCT_PACKAGES += android.hardware.light@2.0-service.xiaomi_msm8994
 
 # LiveDisplay
-PRODUCT_PACKAGES += vendor.lineage.livedisplay@2.0-service-legacymm
+PRODUCT_PACKAGES += vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Media
 PRODUCT_COPY_FILES += \
