3a8f906d2093923877d381898f66c2aa62277810
BYZYB <zyb_1998@outlook.com>
2021-05-21 00:27:09 +0800

    libra: Remove mm-pp-daemon service
    The livedisplay HAL has switched to use mdss driver (adjusts screen color by writing sysfs node "rgb") instead of legacy post processing mode (low performance and needs a "ppd" service) in commit BYZYB/android_device_xiaomi_libra@47fb7cf and BYZYB/android_device_xiaomi_libra@57ed5a7.
    Therefore, "mm-pp-daemon" will not be used by livedisplay HAL (sysfs instead of legacymm mode) and could be removed directly.
    
    This commit also fixes the issue that "mm-pp-daemon" may use 100% power of one CPU core on Android 11.
    
    Change-Id: Iecf430d6e754b2a12754930038907248b511dfba

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-BOARD_VENDOR := xiaomi
 DEVICE_PATH := device/xiaomi/libra
 
 # Arch
@@ -26,9 +25,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a53
-TARGET_USES_64_BIT_BINDER := true
 
 # Assertions
 TARGET_BOARD_INFO_FILE ?= $(DEVICE_PATH)/board-info.txt
@@ -209,9 +206,9 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
 BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB :=lib_driver_cmd_qcwcn
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
 BOARD_WLAN_DEVICE := qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER :=  NL80211
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 TARGET_USES_QCOM_WCNSS_QMI := true
 TARGET_USES_WCNSS_CTRL := true
