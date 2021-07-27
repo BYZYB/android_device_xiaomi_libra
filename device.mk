e370b0078a62ed3030e95f912c3fe59c89ec1ed0
Nolen Johnson <johnsonnolen@gmail.com>
2021-07-27 11:51:41 +0300

    libra: Cleanup dead targets
    Offending entries:
    libQWiFiSoftApCfg
    libgenlock
    build/make/core/main.mk:1136: error: Build failed.

--- device.mk
+++ device.mk
@@ -114,7 +114,6 @@ PRODUCT_PACKAGES += \
     copybit.msm8992 \
     gralloc.msm8992 \
     hwcomposer.msm8992 \
-    libgenlock \
     liboverlay \
     libtinyxml \
     memtrack.msm8992
@@ -292,7 +291,6 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     ipacm-diag \
     libqsap_sdk \
-    libQWiFiSoftApCfg \
     libwpa_client \
     readmac \
     wpa_supplicant \
