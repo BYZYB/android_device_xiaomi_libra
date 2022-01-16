25f5b70d696db11fc346e8583d3d18c466b2fed1
BYZYB <zyb_1998@outlook.com>
2022-01-16 13:34:11 +0800

    libra: Use ZRAM instead of KSM on 2GB devices
    Also see: https://github.com/BYZYB/android_kernel_xiaomi_libra/commit/0482dff67cedd263b0d2344d55fe71a5b951dd24
    
    This reverts commit d8704fdd8895da79fe2eddaaafce4b6e62206d62.
    
    Change-Id: I08b71e6bc20e52184c306dd95c7aeeb7eb7cc8a2

--- rootdir/bin/init.custom.post_boot.sh
+++ rootdir/bin/init.custom.post_boot.sh
@@ -1,6 +1,6 @@
 #!/vendor/bin/sh
 # Enable ADB root when needed
-if [ $(cat /data/adbroot/enabled) -gt 0 ]; then
+if [ "$(cat /data/adbroot/enabled)" -gt 0 ]; then
     setprop service.adb.root 1
     setprop ctl.restart adbd
 fi
@@ -14,3 +14,10 @@ find /data/media/0/tencent/MobileQQ*/*/* -mtime +3 -exec rm -rf {} \;
 find /data/media/0/tencent/MobileQQ*/*/*/* -mtime +3 -exec rm -rf {} \;
 rm -rf /data/anr /data/audio /data/bootchart /data/gsi /data/incremental /data/lineageos_updates /data/local/traces /data/misc/boottrace /data/misc/dash /data/misc/display /data/misc/dts /data/misc/gcov /data/misc/perfprofd /data/misc/prereboot /data/misc/profman /data/misc/qsee /data/misc/SelfHost /data/misc/snapshotctl_log /data/misc/trace /data/misc/update_engine /data/misc/update_engine_log /data/misc/wmtrace /data/nfc /data/ota /data/ota_package /data/per_boot /data/preloads /data/rollback /data/rollback-observer /data/server_configurable_flags /data/ss /data/ssh /data/system/dropbox /data/system/heapdump /data/system/perfd /data/thermal /data/tombstones /data/vendor/tombstones /data/usf
 rm -rf /data/backup/pending/* /data/data/*/app_webview*/BrowserMetrics/* /data/data/*/cache/* /data/data/*/code_cache/* /data/data/com.microsoft.office*/files/Microsoft/Office/* /data/data/com.microsoft.office*/files/temp*/* /data/local/tmp/* /data/media/0/*/.thumbnails/* /data/media/0/Android/data/*/cache/* /data/system/uiderrors.txt /data/user_de/*/*/cache/* /data/user_de/*/*/code_cache/*
+
+# Enable ZRAM on devices with 2GB RAM
+if [ "$(awk '($1 == "MemTotal:") {print $2}' /proc/meminfo)" -lt 2097152 ]; then
+    echo "1G" >/sys/block/zram0/disksize
+    mkswap /dev/block/zram0
+    swapon /dev/block/zram0
+fi
