30f20289b055a59282dab25bd9cbd33538a3aad1
Ricky Cheung <rcheung844@gmail.com>
2022-05-16 16:46:31 +0800

    libra: Uprev to SoundTrigger HAL v2.1
    Change-Id: I77d67bc2978e757304d15a0a2efeb2ec9c7100c9

--- device.mk
+++ device.mk
@@ -42,7 +42,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8992 \
     audio.r_submix.default \
