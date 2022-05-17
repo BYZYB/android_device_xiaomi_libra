73e018071cca660e9de4af2d6e9237d3a9ff4195
BYZYB <zyb_1998@outlook.com>
2022-05-17 21:39:36 +0800

    Revert "libra: Drop QTI power HAL"
    Needed by DT2W, bring it back currently.
    
    This reverts commit b3474cd7913551e00fa6433796bf064dd99d06a4.

--- device.mk
+++ device.mk
@@ -227,6 +227,7 @@ PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.consumerir.xml
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
 # Ramdisk
